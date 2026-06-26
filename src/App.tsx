9e4a6d0b4bdcdccb818aa34f4f1918f34a61eaaa
Deepak Mokal <deepak.mokal@sovos.com>
2026-06-26 16:11:10 +0530

    test commit

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,9 @@ function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">App start working..!</h1>
+      <button className="bg-blue-500 text-white p-2 rounded-md">
+        Click me
+      </button>
     </>
   );
 }
